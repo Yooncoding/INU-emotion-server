d689a9117b32c453e9a5e1412d1422c8915f95fd
윤철중 <cjfwnd123zz@gmail.com>
2021-02-16 04:55:10 +0900

    Add: 로그인 기능(임시)

---
+++ middlewares/auth.middleware.js
@@ -0,0 +1,19 @@
+const { verify } = require("jsonwebtoken");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+module.exports = {
+  verify: (req, res, next) => {
+    const bearerToken = req.headers.authorization;
+    if (!bearerToken) {
+      res.status(403).json({ success: false, message: "로그인이 필요합니다." });
+    } else {
+      const auth = bearerToken.split(" ");
+      if (auth[0] === "Bearer" || auth[0] === "Token") {
+        req.user = verify(auth[1], process.env.JWT_SECRET);
+        next();
+      } else res.status(401).json({ success: false, message: "권한이 없습니다." });
+    }
+  },
+};
--- routes/auth.router.js
+++ routes/auth.router.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
 const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
 
 const router = express.Router();
+dotenv.config();
 
 /**
  * @description 회원가입
@@ -30,6 +33,34 @@ router.post("/register", async (req, res, next) => {
     next(error);
   } finally {
     if (!result.success) res.status(409).json(result);
+    else res.status(201).json(result);
+  }
+});
+
+/**
+ * @description 로그인
+ * @route POST /auth/login
+ * @TODO 회원가입 방법 결정되면 vaildator만들기
+ */
+router.post("/login", async (req, res, next) => {
+  const { email, password } = req.body;
+  let result;
+  try {
+    const user = await User.findOne({ where: { email } });
+    if (!user) result = { success: false, message: "존재하지 않는 이메일 입니다." };
+    else {
+      if (await bcrypt.compare(password, user.password)) {
+        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET);
+        result = { success: true, message: "로그인 성공", token };
+      } else {
+        result = { success: false, message: "비밀번호가 일치하지 않습니다." };
+      }
+    }
+  } catch (error) {
+    console.error(error);
+    next(error);
+  } finally {
+    if (!result.success) res.status(400).json(result);
     else res.status(201).json(result);
   }
 });
