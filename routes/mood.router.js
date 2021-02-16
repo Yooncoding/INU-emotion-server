9e31f15f6f1e11f2d8033e926e12296831eb21b1
윤철중 <cjfwnd123zz@gmail.com>
2021-02-16 16:20:12 +0900

    Add: 오늘 온도 평균 보여주기 기능

--- routes/mood.router.js
+++ routes/mood.router.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const Sequelize = require("sequelize");
 const { isLoggIned } = require("../middlewares/auth.middleware");
 const { isSubmitedByToday } = require("../middlewares/mood.validator");
 const Mood = require("../models/mood");
@@ -36,8 +37,45 @@ router.post("/submit", isSubmitedByToday, async (req, res, next) => {
 /**
  * @description 오늘 온도 평균 보여주기
  * @route GET /mood
- * @TODO
+ * @TODO 전체적으로 모든 라우터 리팩토링하기
+ * @TODO 요소 순위 기능 구현
  */
-router.get("/", (req, res, next) => {});
+router.get("/", async (req, res, next) => {
+  let result;
+  const Op = Sequelize.Op;
+  const TODAY_START = new Date().setHours(0, 0, 0, 0);
+  const NOW = new Date();
+  try {
+    const todayMoodSum = await Mood.sum("select_mood", {
+      where: {
+        createdAt: {
+          [Op.gt]: TODAY_START,
+          [Op.lt]: NOW,
+        },
+      },
+    });
+    const todayMoodNum = await Mood.findAll({
+      where: {
+        createdAt: {
+          [Op.gt]: TODAY_START,
+          [Op.lt]: NOW,
+        },
+      },
+    });
+    const todayMood = parseInt(todayMoodSum / todayMoodNum.length);
+    if (!todayMoodNum) {
+      result = { success: false, message: "아직 오늘 제출된 온도가 없습니다." };
+    } else {
+      // TODO: 여기에 요소 순위 기능 추가할 것
+      result = { success: true, message: `오늘의 온도: ${todayMood}`, todayMood };
+    }
+  } catch (error) {
+    console.error(error);
+    next(error);
+  } finally {
+    if (!result.success) res.status(403).json(result);
+    else res.status(201).json(result);
+  }
+});
 
 module.exports = router;
