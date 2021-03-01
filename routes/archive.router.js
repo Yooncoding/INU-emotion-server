4b90586e2eaa25643cf39b9ea82024018f74c9d0
윤철중 <cjfwnd123zz@gmail.com>
2021-03-01 21:25:02 +0900

    Add: 이번달 온도 보여주기(임시)

--- routes/archive.router.js
+++ routes/archive.router.js
@@ -7,8 +7,33 @@ const router = express.Router();
 /**
  * @description 주간, 월간
  * @route /archive
- * @TODO
+ * @TODO 어떤 형식으로 보내줄지 회의 후 결정
  */
-router.get("/", async (req, res, next) => {});
+router.get("/", async (req, res, next) => {
+  const NOW = new Date();
+  const Op = Sequelize.Op;
+  const MONTH_START = new Date().setDate(0);
+  const archive = [];
+  try {
+    const monthMood = await Archive.findAll({
+      where: {
+        date: {
+          [Op.gt]: MONTH_START,
+          [Op.lt]: NOW,
+        },
+      },
+    });
+    for (let i = 0; i < monthMood.length; i++) {
+      archive.push([monthMood[i].dataValues.date.toString(), monthMood[i].dataValues.mood]);
+    }
+    result = { success: true, message: "이번달 온도", archive };
+  } catch (error) {
+    console.error(error);
+    next(error);
+  } finally {
+    if (!result.success) res.status(403).json(result);
+    else res.status(201).json(result);
+  }
+});
 
 module.exports = router;
